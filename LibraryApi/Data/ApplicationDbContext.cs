b5e03c6747f3d5151130b3768e8790d76530fb25
JaroslawX <ad.jaro@wp.pl>
2024-09-06 11:31:35 +0200

    Add DbContext

---
+++ LibraryApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using LibraryApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApi.Data;
+
+public class ApplicationDbContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
+{
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+}
