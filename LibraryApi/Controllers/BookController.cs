e0968046a12255a0a29624379a5c765a833fe94e
JaroslawX <ad.jaro@wp.pl>
2024-09-06 11:46:46 +0200

    Add book controller

---
+++ LibraryApi/Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using LibraryApi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApi.Controllers;
+
+[Route("LibraryApi/book")]
+[ApiController]
+public class BookController(ApplicationDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var books = context.Books.ToList();
+
+        return Ok(books);
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetById([FromRoute] int id)
+    {
+        var book = context.Books.Find(id);
+
+        if (book == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(book);
+    }
+}
--- LibraryApi/Program.cs
+++ LibraryApi/Program.cs
@@ -25,4 +25,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapControllers();
+
 app.Run();
