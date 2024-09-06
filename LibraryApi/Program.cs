694a208e86d53c4cad1b95700223ef59c95b8fb8
JaroslawX <ad.jaro@wp.pl>
2024-09-06 12:26:50 +0200

    Add seeddata

--- LibraryApi/Program.cs
+++ LibraryApi/Program.cs
@@ -16,6 +16,16 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
+// Seedowanie danych po starcie aplikacji
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+
+    // Wywołanie metody SeedData.Initialize, aby dodać rekordy do bazy danych
+    SeedData.Initialize(context);
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
