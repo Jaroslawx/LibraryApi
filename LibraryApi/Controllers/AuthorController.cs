083609fc69ec718a4b52c4f8c47b05a31dbfdcec
JaroslawX <ad.jaro@wp.pl>
2024-09-09 10:14:50 +0200

    AuthorController

---
+++ LibraryApi/Controllers/AuthorController.cs
@@ -0,0 +1,32 @@
+﻿using LibraryApi.Data;
+using LibraryApi.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApi.Controllers;
+
+[Route("LibraryApi/author")]
+[ApiController]
+public class AuthorController (ApplicationDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var authors = context.Authors.ToList()
+            .Select(s => s.ToAuthorDto());
+
+        return Ok(authors);
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetById([FromRoute] int id)
+    {
+        var author = context.Authors.Find(id);
+
+        if (author == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(author.ToAuthorDto());
+    }
+}
