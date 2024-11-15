0dfb89a88df9b0b8ca8bd39834d0b2433a913b06
JaroslawX <ad.jaro@wp.pl>
2024-11-15 11:49:08 +0100

    New model UserBook

---
+++ LibraryApi/Models/UserBook.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using LibraryApi.Data.Enum;
+
+namespace LibraryApi.Models;
+
+public class UserBook
+{
+    [Key]
+    public int Id { get; init; }
+    
+    [ForeignKey("Book")]
+    public int BookId { get; set; }
+    public Book? Book { get; set; }
+    
+    [Required]
+    public string UserId { get; set; }
+    
+    [Required]
+    public BookStatus Status { get; set; }
+    
+    public DateTime? UpdateDate { get; set; }
+    
+    
+}
