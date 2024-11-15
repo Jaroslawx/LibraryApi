be507619089dd8fc7d9c143db88dd985e7e8b108
JaroslawX <ad.jaro@wp.pl>
2024-11-15 11:58:38 +0100

    BookDto update

--- LibraryApi/Dtos/Book/BookDto.cs
+++ LibraryApi/Dtos/Book/BookDto.cs
@@ -6,9 +6,13 @@ namespace LibraryApi.Dtos.Book;
 public class BookDto
 {
     public int Id { get; set; }
+    public string BookKey { get; set; }
     public string? Title { get; set; }
-    public BookGenre Genre { get; set; }
+    public string[]? Subjects { get; set; }
+    public DateTime? PublishedDate { get; set; }
+    public string? Description { get; set; }
     public int AuthorId { get; set; }
     public Models.Author? Author { get; set; }
     public ICollection<Models.Review>? Reviews { get; set; }
+    public decimal AverageRating { get; set; }
 }
--- LibraryApi/Dtos/Book/UpdateBookRequestDto.cs
+++ LibraryApi/Dtos/Book/UpdateBookRequestDto.cs
@@ -4,7 +4,10 @@ namespace LibraryApi.Dtos.Book;
 
 public class UpdateBookRequestDto
 {
+    public string BookKey { get; set; }
     public string? Title { get; set; }
-    public BookGenre Genre { get; set; }
+    public string[]? Subjects { get; set; }
+    public DateTime? PublishedDate { get; set; }
+    public string? Description { get; set; }
     public int AuthorId { get; set; }
 }
