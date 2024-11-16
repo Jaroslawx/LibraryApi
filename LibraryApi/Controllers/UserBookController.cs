1f8d2b495fac359592dc6c861ccc77287e26a555
JaroslawX <ad.jaro@wp.pl>
2024-11-16 09:05:22 +0100

    UserBook controller and mappers

---
+++ LibraryApi/Controllers/UserBookController.cs
@@ -0,0 +1,19 @@
+﻿using LibraryApi.Interfaces;
+using LibraryApi.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApi.Controllers;
+
+[Route("LibraryApi/userbook")]
+[ApiController]
+public class UserBookController(IUserBookRepository userBookRepo) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var userBooks = await userBookRepo.GetAllAsync();
+        var userBooksDto = userBooks.Select(s => s.ToUserBookDto());
+
+        return Ok(userBooksDto);
+    }
+}
---
+++ LibraryApi/Mappers/UserBooksMappers.cs
@@ -0,0 +1,23 @@
+﻿using LibraryApi.Dtos.UserBook;
+using LibraryApi.Models;
+
+namespace LibraryApi.Mappers;
+
+public static class UserBooksMappers
+{
+    public static UserBookDto ToUserBookDto(this UserBook userBook)
+    {
+        return new UserBookDto
+        {
+            Id = userBook.Id,
+            BookId = userBook.BookId,
+            Book = userBook.Book,
+            UserId = userBook.UserId,
+            Status = userBook.Status,
+            Note = userBook.Note,
+            UpdateDate = userBook.UpdateDate
+        };
+    }
+    
+    
+}
