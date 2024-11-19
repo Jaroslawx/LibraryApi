4cdb8ea4cdfd4ac739f03e4098480c39a9116b95
JaroslawX <ad.jaro@wp.pl>
2024-11-19 15:12:34 +0100

    ApplicationDbContext update

--- LibraryApi/Data/ApplicationDbContext.cs
+++ LibraryApi/Data/ApplicationDbContext.cs
@@ -3,8 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LibraryApi.Data;
 
-public class ApplicationDbContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
-{
+public class ApplicationDbContext : DbContext
+{   
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions)
+        : base(dbContextOptions)
+    {
+    }
+    
     public DbSet<Author> Authors { get; set; }
     public DbSet<Book> Books { get; set; }
     public DbSet<Review> Reviews { get; set; }
